a4f585ade63a295e383a8fced6888966c177d006
dedevelopedby-sam <developedbysam@gmail.com>
2024-09-12 12:27:18 -0500

    initialized repo for the inboXpert chrome-extension

---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "public/manifest.json",
+          dest: ".",
+        },
+        {
+          src: "public/assets/hello.png",
+          dest: "assets/static/",
+        },
+      ],
+    }),
+  ],
+  build: {
+    outDir: "build",
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+      },
+    },
+  },
+});
